f7f1e04f3516f5129da611e94df28b7326ef51a9
Glauber <glaubersantiagodecastro@gmail.com>
2023-09-12 17:04:40 -0300

    MODIFICAÇAO DE SENHA DO BANCO 8 && Package

--- src/server.js
+++ src/server.js
@@ -388,7 +388,7 @@ app.use(cors()); // Isso permitirá todas as origens
 //Criando um cliente para conexão com o PostgreSQL
 const pgClient = new Client({
   user: "postgres",
-  host: "186.244.87.34",
+  host: "pix.ckymwbkfxdvf.sa-east-1.rds.amazonaws.com",
   database: "pedepede",
   password: "12341234",
   port: 5432, // Porta padrão do PostgreSQL
