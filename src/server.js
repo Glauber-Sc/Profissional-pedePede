fdbfe7614f38b625455fdd9746da920f83e876af
Glauber <glaubersantiagodecastro@gmail.com>
2023-09-15 13:08:17 -0300

    troca de ip 32

--- src/server.js
+++ src/server.js
@@ -392,7 +392,7 @@ const pgClient = new Client({
   database: "postgres",
   password: "12341234",
   port: 5432, // Porta padrão do PostgreSQL
-  ssl: true,
+  ssl: false,
 });
 pgClient.connect(); // Conectando ao banco de dados
 
