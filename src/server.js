736a5c6522b915693bb44a3c2dbfd520112d95e3
Glauber <glaubersantiagodecastro@gmail.com>
2023-09-15 00:11:33 -0300

    troca de ip 29

--- src/server.js
+++ src/server.js
@@ -388,7 +388,7 @@ app.use(cors()); // Isso permitirá todas as origens
 //Criando um cliente para conexão com o PostgreSQL
 const pgClient = new Client({
   user: "postgres",
-  host: "172.31.5.76",
+  host: "postgres.ckymwbkfxdvf.sa-east-1.rds.amazonaws.com",
   database: "postgres",
   password: "12341234",
   port: 5432, // Porta padrão do PostgreSQL
