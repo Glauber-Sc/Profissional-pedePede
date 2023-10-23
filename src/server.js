4faa58baeafa9c74cc0f1c48cb55f76230c01e2b
Glauber <glaubersantiagodecastro@gmail.com>
2023-10-23 13:26:50 -0300

    ajuste txid para o status de pago no webhook 12

--- src/server.js
+++ src/server.js
@@ -123,7 +123,6 @@ app.post("/pix", async (req, res) => {
     const ordersQuery = `
     UPDATE public.orders
     SET txid = $1
-    WHERE created_at = (SELECT MAX(created_at) FROM public.orders);    
   `;
 
     const ordersValues = [cobResponse.data.txid];
